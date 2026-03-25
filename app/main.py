64847aa56b3dab51b11822d07bd38a8ade843b97
A Jeselle <aly.yanga.03@gmail.com>
2026-03-25 19:03:34 +0800

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -38,18 +38,15 @@ def main():
                 else:
                     print(f"{command[5:]}: not found")
         else:
+            found = False
             for directory in PATH:
-                full_path = os.path.join(directory, parts[0])
-                if os.path.exists(full_path):
-                    if not os.access(full_path, os.X_OK):
-                        continue
-                    else:
-                        # print(f"{command} is {full_path}")
-                        subprocess.run(program_name + args, executable=full_path)
-                        break
-                else:
-                    print(f"{command}: command not found")
+                full_path = os.path.join(directory, program_name)
+                if os.path.exists(full_path) and os.access(full_path, os.X_OK):
+                    subprocess.run([program_name] + args, executable=full_path)
+                    found = True
                     break
+            if not found:
+                print(f"{command}: command not found")
 
 if __name__ == "__main__":
     main()
